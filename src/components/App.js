ada72bcca1a56d4ed8260f398eed38aec0d2f064
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-08-02 10:25:58 +0200

    add

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Note from './Note/Note';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <Note />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Note/Note.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Note = () => {
+  return (
+    <div>
+      <h2>Single Note</h2>
+    </div>
+  );
+};
+
+export default Note;
